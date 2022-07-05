4c63e161b59c6cd9b5eb257578e5a0cdb1a7e2af
lliFoXill <fox.ruden@gmail.com>
2022-07-05 17:16:54 +0300

    end

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -3,6 +3,10 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileMenuNav = document.querySelector("[data-menu-nav]");
   const mobileMenuAuth = document.querySelector("[data-menu-auth]");
+  const body = document.querySelector('body');
+  
+
+  
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -10,9 +14,25 @@
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
+     body.classList.toggle('no-scroll');
 
     mobileMenuRef.classList.toggle("is-open");
     mobileMenuNav.classList.toggle("is-open");
     mobileMenuAuth.classList.toggle("is-open");
+
   });
 })();
+  const toggleMenu = () => {
+    const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    
+    
+  };
